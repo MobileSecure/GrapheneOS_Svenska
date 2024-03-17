ce32d172b73aaacf476c94e666c340ad0b0d793b
MobileSecure <163734133+MobileSecure@users.noreply.github.com>
2024-03-17 19:40:02 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+st.markdown("""
+            # GrapheneOS
+Introduktion av vår GrapheneOS-säkerhets- och integritetstjänst:
+
+- **Förbättrad Integritet och Säkerhet med Användarprofiler:** GrapheneOS erbjuder avancerade användarprofiler, vilket ger ett säkert och privat sätt att isolera olika aspekter av ditt digitala liv. 🔒
+- **Förebyggande av Appbeteende:** Separata profiler säkerställer att appbeteenden eller skadliga appar inte kan påverka din huvudprofil, vilket håller dina data säkra och skyddade. ⚠️
+- **Enkel Aktivering och Migration:** Aktivering av användarprofiler är enkelt med GrapheneOS-inställningar, och du kan smidigt migrera befintliga appar till nya profiler för ökad bekvämlighet och säkerhet. ✅
+- **Skydd av Privata Data:** Separata profiler är ovärderliga när du delar din telefon med andra, vilket säkerställer att dina privata data förblir skyddade vid alla tillfällen.
+- **Optimerade Nätverks- och Wi-Fi-inställningar:** Vi rekommenderar specifika nätverks- och Wi-Fi-inställningar, såsom att stänga av automatisk Wi-Fi-skanning och anpassa hotspot-namn och lösenord, för att förbättra säkerheten och integriteten. 📶
+- **Personliga Bluetooth- och Enhetsinställningar:** GrapheneOS tillåter dig att anpassa enhetsnamn och justera Bluetooth-inställningar för ökad integritet och bekvämlighet.
+- **Förstärkta Säkerhetsåtgärder:** Att inaktivera NFC och molnutskrift kan ytterligare stärka säkerheten och minska potentiella sårbarheter. 🚫
+- **Anpassade Notifikations- och Skärminställningar:** Finjustering av notifikationsinställningar, batteriprocentagesdisplay och aktivering av dubbla Scopes säkerställer en personlig och säker användarupplevelse. 📵
+- **Granulära Integritetskontroller:** Hantera sensorbehörigheter, kontrollera kamera- och mikrofontillgång och reglera urklipps synlighet för att effektivt skydda din integritet. 🔐
+- **Skräddarsydda Säkerhetsåtgärder:** Välj mellan PIN-kod och fingeravtryckssäkerhetsalternativ baserat på dina hotmodeller och personliga preferenser, vilket säkerställer högsta skyddsnivå för din enhet och data. 🔐
+- Upplev tryggheten som kommer med GrapheneOS-säkerhets- och integritetstjänsten - skydda ditt digitala liv med toppmodern teknik och enastående skydd.
+
+### **GrapheneOS**
+
+Det privata och säkra mobila operativsystemet med Android-appkompatibilitet. Utvecklat som ett ideellt öppen källkodsprojekt.
+
+### Sammanfattning 
+
+GrapheneOS är som en superladdad sköld för din smartphone, vilket gör den super säker och privat. Det är som att ha en livvakt för din telefon mot hackare och nyfikna.
+
+Startat 2014, kallades det ursprungligen CopperheadOS. Men nu fokuserar GrapheneOS på att göra din telefon extra säker genom att fixa många vanliga problem som hackare kan använda för att bryta sig in. Det är som att lägga till extra lås och larm till din telefon.
+
+När du använder GrapheneOS, är det som att din telefon blir en fästning. Den är utformad för att hålla dina personliga saker säkra, som foton, meddelanden och lösenord, från nyfikna ögon. Plus, den har inga lömska Google-appar eller tjänster som spionerar på dig.
+
+Och det bästa? Du behöver inte vara en teknikexpert för att använda den. GrapheneOS försöker göra det enkelt, så att du kan hålla dig säker utan att behöva oroa dig för komplicerade inställningar eller alternativ. Det är som att ha en tuff livvakt som arbetar tyst i bakgrunden, håller dig säker utan att du ens märker det.
+
+#### Vilka enheter **stöds**?
+GrapheneOS har officiellt produktionsstöd för följande enheter:
+
+- Pixel 8 Pro (husky)
+- Pixel 8 (shiba)
+- Pixel Fold (felix)
+- Pixel Tablet (tangorpro)
+- Pixel 7a (lynx)
+- Pixel 7 Pro (cheetah)
+- Pixel 7 (panther)
+- Pixel 6a (bluejay)
+- Pixel 6 Pro (raven)
+- Pixel 6 (oriole)
+- Pixel 5a (barbet)""")
